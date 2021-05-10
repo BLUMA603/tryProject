75e72071b2cb3e57532b245990d7cd93670b61a3
BLUMA603 <58975702+BLUMA603@users.noreply.github.com>
2021-05-10 18:03:22 +0300

    Update Startup.cs

--- try/Startup.cs
+++ try/Startup.cs
@@ -32,7 +32,7 @@ namespace myTry
         {
             endpoints.MapGet("/", async context =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Hello World!!!!!");
             });
         });
     }
